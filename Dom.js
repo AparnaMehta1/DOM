5e6fb72904fec419d2e39f361cf6d901e7294690
Aparna <AparnaMehta1>
2022-11-21 14:29:26 +0530

    first commit

---
+++ Dom.js
@@ -0,0 +1,13 @@
+//use dot notation with the object to access any value for example  
+
+console.log(document.title);
+console.log(document.body);
+console.log(document.domain);
+console.log(document.head);
+console.log(document);
+
+//DOM- Dom is an object. which uses for grabbing the element and manipulating the element
+// 1. Grabbing the element - Methods for grabbing the element 
+// i)  document.getElementById- uses for grabbing element through ID it ask for 1 parameter ID.
+console.log(document.getElementById("header-title"));
+
